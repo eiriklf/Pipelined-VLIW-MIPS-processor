87621f9a98f67bb54ae7b0ca20da2316603264bc
Eirik Flogard <eiriklf@hotmail.com>
2013-10-31 08:33:31 +0100

    Update Vliw_alu.vhd
    added division operation. must add hi-register result

--- exersise2experiment/Vliw_alu.vhd
+++ exersise2experiment/Vliw_alu.vhd
@@ -38,19 +38,26 @@ use WORK.MIPS_CONSTANT_PKG.ALL;
 
 entity Vliw_alu is
 	generic (N: NATURAL);
-	port(
-		X			: in STD_LOGIC_VECTOR(N-1 downto 0);
-		Y			: in STD_LOGIC_VECTOR(N-1 downto 0);
-		R			: out STD_LOGIC_VECTOR(N-1 downto 0)
-	);
+        port(
+                X                        : in STD_LOGIC_VECTOR(N-1 downto 0);
+                Y                        : in STD_LOGIC_VECTOR(N-1 downto 0);
+                R                        : out STD_LOGIC_VECTOR(N-1 downto 0);
+                vliw_aluOP                : in std_logic;
+        );
 end Vliw_alu;
 
 architecture Behavioral of Vliw_alu is
-	signal multiplied: std_logic_vector(63 downto 0);
+	signal result: std_logic_vector(63 downto 0);
 begin
+process(vliw_aluOP)
+begin
+			if(vliw_aluOP='0') then
+			result<=X*Y;
+			else
+			result<=X/Y;
+			end if;
+end process;
 
-			multiplied<=X*Y;
-			R<=multiplied(31 downto 0);
-
+			R<=result(31 downto 0);
 end Behavioral;
 
